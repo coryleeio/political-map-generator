1236bd07b68b739ce185f2a3c693ae8d3c575939
Cory Lee <cl10001123@gmail.com>
2015-03-07 02:07:07 -0600

    Fix a few bugs and add debugging

--- js/mappoc.js
+++ js/mappoc.js
@@ -3,7 +3,7 @@ var MapProc = {
     diagram: null,
     margin: 0.0,
     canvas: null,
-    numberOfNodes: 1000,
+    numberOfSites: 1000,
     cellsForPlate: [],
     plateToContains: [],
     bbox: {
@@ -12,7 +12,7 @@ var MapProc = {
         yt: 0,
         yb: 600
     },
-    numberOfPlates: 10,
+    numberOfPlates: 14,
     plateRootIds: [],
     sites: [],
     numberOfIterations: 2,
@@ -22,7 +22,7 @@ var MapProc = {
 
     init: function() {
         this.canvas = document.getElementById('voronoiCanvas');
-        this.randomSites(this.numberOfNodes, true);
+        this.randomSites(this.numberOfSites, true);
         for (var i = 0; i < this.numberOfIterations; i++) {
             this.relaxSites();
         }
@@ -55,13 +55,8 @@ var MapProc = {
             colorByPlate[plateNumber] = this.randomHexColor();
         }
 
-        for(var cellId=0; cellId < this.numberOfNodes; cellId++ ) {
+        for(var cellId=0; cellId < this.getCellCount(); cellId++ ) {
             var cell = this.getCellForId( cellId );
-            if(cell == null)
-            {
-                console.log("culprit");
-                console.log(cellId);
-            }
             if( cell != null && cell.plate != null ) {
                 cell.cellColor = colorByPlate[cell.plate];
             }
@@ -72,7 +67,7 @@ var MapProc = {
     renderStainedGlassView: function() {
         this.renderMapBorder();
         Voronoi.prototype.Cell.prototype.edgeColor='#333';
-        for(var cellId=0; cellId < this.numberOfNodes; cellId++ ) {
+        for(var cellId=0; cellId < this.getCellCount(); cellId++ ) {
             var cell = this.getCellForId( cellId );
             if( cell != null ) {
                 var color = this.randomHexColor();
@@ -85,22 +80,32 @@ var MapProc = {
     },
 
     renderAllCells: function() {
-        for (var i =0; i<this.numberOfNodes; i++) {
+        for (var i =0; i<this.getCellCount(); i++) {
             this.renderCell(i);
         }
     },
 
     assignPlates: function() {
         this.plateRootIds = this.selectPlateStartingCellIds();
+        console.log("selected Plate root ids = " + this.plateRootIds);
         // Timeslice between multi-site random flood-fill.
         // Store nodes that might be valid expansion points here.
+        console.log("Sanity node count is " + this.getCellCount());
         var plateToValidAdjacent = [];
-        while(this.hasNodesNotAssignedToPlates()) {
-            for(var nodesAssigned = 0; nodesAssigned < this.numberOfNodes; nodesAssigned++ ) {
+        // while(this.hasNodesNotAssignedToPlates()) {
+            var cellsAssigned = 0;
+            while(cellsAssigned < this.getCellCount())
+            {
+                console.log("starting cell assigned loop.." + cellsAssigned);
                 for(var plateNumber=0; plateNumber < this.plateRootIds.length; plateNumber++) {
+                    console.log("starting plateNumber loop.." + cellsAssigned);
                    // Unsure datastore is initialized.
                    if(plateToValidAdjacent[plateNumber] == null) {
                         plateToValidAdjacent[plateNumber] = [];
+                   }
+
+                   if(this.plateToContains[plateNumber] == null)
+                   {
                         this.plateToContains[plateNumber] = [];
                    }
 
@@ -108,12 +113,20 @@ var MapProc = {
                         // Don't have a start node yet, get one
                         var cellId = this.plateRootIds[plateNumber];
                         this.getCellForId(cellId).plate = plateNumber;
+                        cellsAssigned++;
                         this.plateToContains[plateNumber].push(cellId);
                         plateToValidAdjacent[plateNumber] = this.findAdjacentCells(cellId);
+                        console.log("setting plate start node for " + plateNumber);
+                        console.log("root is " + cellId);
+                        console.log("plate " + plateNumber + " now contains " + this.plateToContains[plateNumber]);
+                        console.log("plate " + plateNumber + " is adjacent to " + plateToValidAdjacent[plateNumber]);
+                        console.log("done setting plate start node for " + plateNumber);
+                        continue;
                    }
 
                    if(plateToValidAdjacent[plateNumber].length == 0)
                    {
+                        console.log("plate is done growing - " + plateNumber);
                         // this plate is done growing
                         continue;
                    }
@@ -122,21 +135,45 @@ var MapProc = {
                    var cellIdToTry = plateToValidAdjacent[plateNumber][cellArrayIndexToTry];
                    var cellId = cellIdToTry;
                    var cell = this.getCellForId(cellId);
-
+                   console.log("Decided to try " + cellId);
+                   console.log("cell is here " + cell);
                    if(cell.plate == null) {
+                        console.log("this cell had no plate so we set his plate now.");
                         cell.plate = plateNumber;
+                        cellsAssigned++;
+                        console.log("cell plate = " + cell.plate);
+
+
                         this.plateToContains[plateNumber].push(cellId);
-                        plateToValidAdjacent[plateNumber] = this.mergeArray(plateToValidAdjacent[plateNumber], this.findAdjacentCells(cellId));
+                        console.log("plate " + plateNumber + " now contains " + this.plateToContains[plateNumber]);
+                        var adjacents = this.findAdjacentCells(cellId);
+                        console.log("Valid adjacents for plateNumber " + plateNumber + " premerge = " + plateToValidAdjacent[plateNumber]);
+                        console.log("Nodes adjacent to cell: " + cellId + " include " + adjacents);
+                        plateToValidAdjacent[plateNumber] = this.mergeArray(plateToValidAdjacent[plateNumber], adjacents);
                         
+                        console.log("post plate adjacency merge plate " + plateNumber + "is adjacent to " + plateToValidAdjacent[plateNumber]);
+
+
+                        console.log("Now we remove the contents of the plate " + plateNumber + " from the nodes adjacent to the plate");
+                        console.log("premerge plate contents = " + this.plateToContains[plateNumber]);
+                        console.log("premerge plate adjacents = "+ plateToValidAdjacent[plateNumber]);
+
                         for (value in this.plateToContains[plateNumber]) {
                             plateToValidAdjacent[plateNumber] = plateToValidAdjacent[plateNumber].filter(function(element){
                                 return element !== value;
                             }); // ensure no adjacent plates have already been added.
                         }
+                        console.log("postmerge plate contents = " + this.plateToContains[plateNumber]);
+                        console.log("postmerge plate adjacents = "+ plateToValidAdjacent[plateNumber]);
+                        console.log("done with plate " + plateNumber + " for now");
+                        console.log(" XXXXXXXXXXXXXXXXXXXXXXX");
+                   }
+                   else {
+                        console.log("ALREADY had a plate! going to next node!");
                    }
                 }
             }
-        }
+        // }
     },
 
     hasNodesNotAssignedToPlates: function() {
@@ -147,7 +184,6 @@ var MapProc = {
                 return true;
             }
         }
-
     },
 
     // merge two arrays purging duplicates, 
@@ -207,17 +243,20 @@ var MapProc = {
 	    return false;
 	},
 
-    selectPlateStartingCellIds: function(numberToSelect) {
+    selectPlateStartingCellIds: function() {
     	var arr = [];
+        console.log("Selecting starting plates...");
     	var i=0;
     	while(i < this.numberOfPlates) {
-    		var randomNumber = this.randomIntegerInRange(0,this.numberOfNodes - 1);
+    		var randomNumber = this.randomIntegerInRange(0,this.getCellCount() - 1);
 	    	if(!this.inArray(arr, randomNumber))
 	    	{
 	    		arr.push(randomNumber);
 	    		i++;
 	    	}
 	    }
+        console.log("number of plates returned = " + arr.length);
+        console.log("number of plates requested = " + this.numberOfPlates);
 	    return arr;
     },
 
@@ -255,6 +294,10 @@ var MapProc = {
             });
         }
         this.compute(sites);
+    },
+
+    getCellCount: function() {
+        return this.diagram.cells.length;
     },
 
     relaxSites: function() {
@@ -343,8 +386,8 @@ var MapProc = {
         var treemap = new QuadTree({
             x: this.bbox.xl,
             y: this.bbox.yt,
-            width: this.numberOfNodes * 5,
-            height: this.numberOfNodes * 5
+            width: this.numberOfSites * 7,
+            height: this.numberOfSites * 7
         });
         var cells = this.diagram.cells,
             iCell = cells.length;
@@ -424,28 +467,36 @@ var MapProc = {
     },
 
     renderCell: function(id) {
+        console.log("renderCell " + id);
         if (id === undefined) {
+            console.log("1")
             return;
         }
         if (!this.diagram) {
+            console.log("2")
             return;
         }
         var cell = this.getCellForId(id);
         if (!cell) {
+            console.log("3")
             return;
         }
         this.renderCellWithColor(id, cell.cellColor, cell.edgeColor, cell.siteColor);
     },
 
     renderCellWithColor: function(id, cellColor, edgeColor, siteColor) {
+        console.log("renderCellWithColor " + id + " " + cellColor + " " + edgeColor + " " + siteColor);
         if (id === undefined) {
+            console.log("4")
             return;
         }
         if (!this.diagram) {
+            console.log("5")
             return;
         }
         var cell = this.getCellForId(id);
         if (!cell) {
+            console.log("6")
             return;
         }
         var drawingContext = this.canvas.getContext('2d');
